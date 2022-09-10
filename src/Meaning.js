c5b11741e25bd05c1eff6229e623ff6776d4cc09
HannaVid <hannaviden@gmail.com>
2022-09-10 16:18:45 +0300

    Added phonetics

--- src/Meaning.js
+++ src/Meaning.js
@@ -6,9 +6,7 @@ export default function Meaning(props) {
   return (
     <div className="Meaning">
       <h3>{props.meaning.partOfSpeech}</h3>
-
       <Synonyms synonyms={props.meaning.synonyms} />
-
       {props.meaning.definitions.map(function (definiton, index) {
         return (
           <div key={index}>
